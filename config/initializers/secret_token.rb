739ed46eed4de3dfa8984cf1ca112b1bc0add300
telaxia <telaxia@operamail.com>
2013-06-24 15:34:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '48309130539ea34813a88cac2116392908abb7b3978eb2fe52fcbaa14d724fe28299c72c65ca5f95e859b647605ef549038445924160e5bc14ca4b1efe84ac62'
